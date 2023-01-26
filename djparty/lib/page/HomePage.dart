0910dbd7f59aead5593f2dc3171d8d5dddf5535f
leo <61985313+giusti-leo@users.noreply.github.com>
2023-01-26 23:46:09 +0100

    small changes

---
+++ djparty/lib/page/HomePage.dart
@@ -0,0 +1,45 @@
+import 'package:djparty/page/Home.dart';
+import 'package:djparty/page/SignIn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(initialRoute: '/', routes: {
+      'homepage': (context) => HomePage(),
+      'login': (context) => const SignIn(),
+    });
+  }
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'App name',
+        home: Builder(builder: (BuildContext context) {
+          return Scaffold(
+              body: FutureBuilder(
+                  future: _initializeFirebase(),
+                  builder: (context, shapshot) {
+                    if (shapshot.connectionState == ConnectionState.done) {
+                      return const Home();
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }));
+        }));
+  }
+}
